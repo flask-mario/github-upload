c289fb351d6de69400af8548116f48368bcefbff
sbx_user1051 <sbx_user1051@169.254.220.61>
2022-07-06 06:08:10 +0000

    Update requested for Proton - environment/app1-dev-env2

---
+++ app1-dev-env2/data.tf
@@ -0,0 +1,21 @@
+/*
+This file is managed by AWS Proton. Any changes made directly to this file will be overwritten the next time AWS Proton performs an update.
+
+To manage this resource, see AWS Proton Resource: arn:aws:proton:ap-northeast-1:027500179340:environment/app1-dev-env2
+
+If the resource is no longer accessible within AWS Proton, it may have been deleted and may require manual cleanup.
+*/
+
+# Partition (commercial, govCloud, etc) in which to deploy the solution
+data "aws_partition" "current" {}
+
+# Find the user currently in use by AWS
+data "aws_caller_identity" "current" {}
+
+# Region in which to deploy the solution
+data "aws_region" "current" {}
+
+# Availability zones to use in our soultion
+data "aws_availability_zones" "available" {
+  state = "available"
+}
