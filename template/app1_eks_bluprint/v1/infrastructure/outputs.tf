ef8372180ff487b5294ddbf8991b7cd948910161
flask-mario <flask@mz.co.kr>
2022-07-12 01:35:06 +0000

    modify output list to string

--- template/app1_eks_bluprint/v1/infrastructure/outputs.tf
+++ template/app1_eks_bluprint/v1/infrastructure/outputs.tf
@@ -4,12 +4,12 @@ output "vpc_id" {
 }
 output "private_subnets" {
   description = "List of IDs of private subnets"
-  value       = module.vpc.private_subnets
+  value       = join(", ", module.vpc.private_subnets)
 }
 
 output "public_subnets" {
   description = "List of IDs of public subnets"
-  value       = module.vpc.public_subnets
+  value       = join(", ", module.vpc.public_subnets)
 }
 
 output "configure_kubectl" {
@@ -19,10 +19,10 @@ output "configure_kubectl" {
 
 output "team_riker" {
   description = "Role Arn of team-riker"
-  value       = module.eks_blueprints.teams[*].application_teams_iam_role_arn["team-riker"]
+  value       = join(", ", module.eks_blueprints.teams[*].application_teams_iam_role_arn["team-riker"])
 }
 
 output "platform_team" {
   description = "Role Arn of platform-team"
-  value       = module.eks_blueprints.teams[*].platform_teams_iam_role_arn["admin"]
+  value       = join(", ", module.eks_blueprints.teams[*].platform_teams_iam_role_arn["admin"])
 }
