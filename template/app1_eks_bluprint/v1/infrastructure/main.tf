b5bbe0437b143bead996f6c4d8f3de1276dd2d6e
flask-mario <flask@mz.co.kr>
2022-07-12 06:49:12 +0000

    ...

--- template/app1_eks_bluprint/v1/infrastructure/main.tf
+++ template/app1_eks_bluprint/v1/infrastructure/main.tf
@@ -93,8 +93,13 @@ module "eks_blueprints" {
   managed_node_groups = {
     mg_5 = {
       node_group_name = "managed-ondemand"
-      instance_types  = ["m5.xlarge"]
+      instance_types  = ["m5.large"]
       subnet_ids      = module.vpc.private_subnets
+      
+      # Node Group scaling configuration
+      desired_size    = 3
+      max_size        = 3
+      min_size        = 3
     }
   }
 
